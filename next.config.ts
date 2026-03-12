a2d12b230b6946c19b52fa5bea64fa52b697fd87
alexchtst <acinatra@gmail.com>
2026-03-13 06:42:56 +0700

    feat: offline support for pwa prototyping

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,43 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  // ganti nanti
+  disable: true,
+  register: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  fallbacks: {
+    document: "/offline",
+  },
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https?:\/\/.*\/.*$/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "pages-cache",
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 60 * 60 * 24 * 7,
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:js|css|png|jpg|jpeg|svg|gif|woff|woff2)$/,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "static-assets",
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 30,
+          },
+        },
+      },
+    ],
+  },
+})(nextConfig);
